626cd65175ee2d7240d26f2f4a116cdadc4beebf
Kevin Borah <kevinborah1994@gmail.com>
2020-10-14 14:27:10 -0400

    Add files via upload
    Updated Tic_Tac_Toe Game

--- Python_Code/Tic_Tac_Toe_v1.py
+++ Python_Code/Tic_Tac_Toe_v1.py
@@ -1,3 +1,6 @@
+import random
+
+
 # Create the Board
 # Choose an Initial Player
 # Until Someone Wins
@@ -10,6 +13,12 @@
 
 
 def main():
+    print("---------------------------")
+    print("        Tic_Tac_Toe")
+    print("Can you get three in a row?")
+    print("---------------------------")
+    print()
+
     # Create the Board
     # Board is a list of rows
     # Rows are a list of columns
@@ -21,8 +30,9 @@ def main():
 
     # Choose an Initial Player
     active_player_index = 0
-    players = ["Kevin", "Computer"]
+    players = get_players()
     symbols = ["X", "O"]
+    player = players[active_player_index]
 
     # Until Someone Wins
     while not find_winner(board):
@@ -32,10 +42,27 @@ def main():
 
         announce_turn(player)
         show_board(board)
-        if not choose_location(board, symbol):
+        if not choose_location(board, symbol, player):
             print("That's not a valid play! Try again.")
             continue
-        input("pause")
+
+        # Toggle Active Player
+        active_player_index = (active_player_index + 1) % len(players)
+
+    print(f"Game Over! {player} Wins the game with the board:   ")
+    show_board(board)
+
+
+def get_players():
+    players = [None, None]
+    players[0] = input("Player 1: what is your name?")
+    print("Player 1: Do you want to play against a Human or the Computer?")
+    player_2 = int(input("(Enter 1 to play against a Human, or enter 2 to play against the Computer)"))
+    if player_2 == 1:
+        players[1] = input("Player 2: What is your name?")
+    else:
+        players[1] = "COMPUTER"
+    return players
 
 
 def announce_turn(player):
@@ -53,27 +80,68 @@ def show_board(board):
         print()
 
 
-def choose_location(board, symbol):
-    row = int(input("Choose which row!"))
-    column = int(input("Choose which column!"))
+def choose_location(board, symbol, active_player):
+    # DECIDING WHETHER THE ACTIVE PLAYER IS THE COMPUTER, IF IT IS THEN IT RANDOMLY SELECTS A BOARD POSITION FOR THE COMPUTER.
+    if active_player == "COMPUTER":
+        row = random.randint(0, len(board) - 1)
+        column = random.randint(0, len(board[row]) - 1)
+        cell = board[row][column]
+        if cell is not None:
+            return False
+        board[row][column] = symbol
+        return True
+    # IF IT'S NOT THE COMPUTER YOU GET TO GO.
+    else:
+        row = int(input("Choose which row!"))
+        column = int(input("Choose which column!"))
 
-    row -= 1
-    column -= 1
-    if row < 0 or row > 2:
-        return False
-    if column < 0 or column > 2:
-        return False
+        row -= 1
+        column -= 1
+        if row < 0 or row > 2:
+            return False
+        if column < 0 or column > 2:
+            return False
 
-    cell = board[row][column]
-    if cell is not None:
-        return False
-    board[row][column] = symbol
-    return True
+        cell = board[row][column]
+        if cell is not None:
+            return False
+        board[row][column] = symbol
+        return True
 
 
 def find_winner(board):
-    # Todo implement how we check for a winner
+    sequences = find_winning_sequences(board)
+    for cells in sequences:
+        symbol1 = cells[0]
+        if symbol1 and all(symbol1 == cell for cell in cells):
+            return True
+
     return False
+
+
+def find_winning_sequences(board):
+    sequences = []
+
+    # WIN BY ROWS
+    rows = board
+    sequences.extend(rows)
+
+    # WIN BY COLUMNS
+    for col_idx in range(0, 3):
+        col = [
+            board[0][col_idx],
+            board[1][col_idx],
+            board[2][col_idx],
+        ]
+        sequences.append(col)
+
+    # WIN BY DIAGONALS
+    diagonals = [
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+    ]
+    sequences.extend(diagonals)
+    return sequences
 
 
 if __name__ == '__main__':
