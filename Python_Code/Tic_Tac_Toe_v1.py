c2278a9ca6b306441512511641a673d187f64ddc
Kevin Borah <kevinborah1994@gmail.com>
2020-10-14 11:32:16 -0400

    Add files via upload
    I've updated Tic_Tac_Toe to have some gameplay but it's still not complete.

--- Python_Code/Tic_Tac_Toe_v1.py
+++ Python_Code/Tic_Tac_Toe_v1.py
@@ -21,14 +21,54 @@ def main():
 
     # Choose an Initial Player
     active_player_index = 0
-    players = ["You", "Computer"]
+    players = ["Kevin", "Computer"]
     symbols = ["X", "O"]
 
     # Until Someone Wins
     while not find_winner(board):
-        print("Play a round...")
+        # SHOW THE BOARD
+        player = players[active_player_index]
+        symbol = symbols[active_player_index]
 
-    print(board)
+        announce_turn(player)
+        show_board(board)
+        if not choose_location(board, symbol):
+            print("That's not a valid play! Try again.")
+            continue
+        input("pause")
+
+
+def announce_turn(player):
+    print()
+    print(f"It's {player}'s turn. Here's the board.")
+    print()
+
+
+def show_board(board):
+    for row in board:
+        print(" | ", end='')
+        for cell in row:
+            symbol = cell if cell is not None else "_"
+            print(symbol, end=' | ')
+        print()
+
+
+def choose_location(board, symbol):
+    row = int(input("Choose which row!"))
+    column = int(input("Choose which column!"))
+
+    row -= 1
+    column -= 1
+    if row < 0 or row > 2:
+        return False
+    if column < 0 or column > 2:
+        return False
+
+    cell = board[row][column]
+    if cell is not None:
+        return False
+    board[row][column] = symbol
+    return True
 
 
 def find_winner(board):
